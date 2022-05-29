562d5e079ef342ff6f08001f6f0e5a3fdbc98f69
moonyoung <moonyoung.dev@kakao.com>
2022-05-29 20:33:05 +0900

    feat: update member set up

--- membership/dto.go
+++ membership/dto.go
@@ -9,3 +9,15 @@ type CreateResponse struct {
 	ID             string
 	MembershipType string
 }
+
+type UpdateRequest struct {
+	ID             string
+	UserName       string
+	MembershipType string
+}
+
+type UpdateResponse struct {
+	ID             string
+	UserName       string
+	MembershipType string
+}
